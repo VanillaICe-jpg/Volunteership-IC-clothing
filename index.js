d48c901cd131e4b8239ecaf2bc545075466ccbb2
Israel Cruz <isracruzm@hotmail.com>
2020-10-14 12:50:37 -0500

    update 10/14/2020 1pm, comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log("hola mundo");
 
+// Nav bar scroll -> navbar color change and adquire "sticky" propiety//
 window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 0);
